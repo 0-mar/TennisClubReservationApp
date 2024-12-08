7aee11d8bbb00bfefeb19b3ed22867562d7dfe1c
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-08 12:11:14 +0100

    feat: initial commit

---
+++ src/main/java/cz/omar/tennisclubreservationapp/TennisClubReservationAppApplication.java
@@ -0,0 +1,13 @@
+package cz.omar.tennisclubreservationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisClubReservationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisClubReservationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/omar/tennisclubreservationapp/TennisClubReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.omar.tennisclubreservationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisClubReservationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
