d601593f1a75a17b696efa4ad10275b3dc88f337
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-09 23:06:12 +0100

    feat: add Court mapper between business and model classes

---
+++ src/main/java/cz/omar/tennisclubreservationapp/court/mappers/CourtToDatabaseMapper.java
@@ -0,0 +1,20 @@
+package cz.omar.tennisclubreservationapp.court.mappers;
+
+import cz.omar.tennisclubreservationapp.court.business.Court;
+import cz.omar.tennisclubreservationapp.court.storage.CourtEntity;
+import cz.omar.tennisclubreservationapp.surface.mappers.SurfaceToDatabaseMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {SurfaceToDatabaseMapper.class})
+public interface CourtToDatabaseMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "surfaceEntity", target = "surface")
+    Court entityToCourt(CourtEntity courtEntity);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "surface", target = "surfaceEntity")
+    CourtEntity courtToEntity(Court court);
+}
