76676fb5f0bbecb34f044239a44aaa15985eca6e
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-09 23:03:21 +0100

    feat: add mapper between business and model surface classes

---
+++ src/main/java/cz/omar/tennisclubreservationapp/surface/mappers/SurfaceToDatabaseMapper.java
@@ -0,0 +1,19 @@
+package cz.omar.tennisclubreservationapp.surface.mappers;
+
+import cz.omar.tennisclubreservationapp.surface.business.Surface;
+import cz.omar.tennisclubreservationapp.surface.storage.SurfaceEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface SurfaceToDatabaseMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "rentPerMinute", target = "rentPerMinute")
+    Surface entityToSurface(SurfaceEntity surfaceEntity);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "rentPerMinute", target = "rentPerMinute")
+    SurfaceEntity surfaceToEntity(Surface surface);
+}
