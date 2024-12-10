5dbd16da4166efabc4eec8645605533925230ebc
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-11 00:11:07 +0100

    refactor: split name to first and last name

--- src/main/java/cz/omar/tennisclubreservationapp/customer/business/Customer.java
+++ src/main/java/cz/omar/tennisclubreservationapp/customer/business/Customer.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Customer {
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String phoneNumber;
 }
--- src/main/java/cz/omar/tennisclubreservationapp/customer/storage/CustomerEntity.java
+++ src/main/java/cz/omar/tennisclubreservationapp/customer/storage/CustomerEntity.java
@@ -10,52 +10,15 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class CustomerEntity extends BaseEntity {
 
-    @Column(name = "name", nullable = false)
-    private String name;
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
 
     @Column(name = "phoneNumber", nullable = false)
     private String phoneNumber;
 
     @Column(name = "deleted", nullable = false)
     private boolean deleted;
-
-    /*public Customer(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.deleted = false;
-    }
-
-    public Customer() {}
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public Long getId() {
-        return id;
-    }*/
 }
