c2ef4269e862c4e10dc93ef63f6c55e21d5d4ce8
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-12 11:49:30 +0100

    feat: add data creation

--- src/main/java/cz/omar/tennisclubreservationapp/TennisClubReservationAppApplication.java
+++ src/main/java/cz/omar/tennisclubreservationapp/TennisClubReservationAppApplication.java
@@ -1,13 +1,35 @@
 package cz.omar.tennisclubreservationapp;
 
+import cz.omar.tennisclubreservationapp.court.business.Court;
+import cz.omar.tennisclubreservationapp.court.storage.CourtEntity;
+import cz.omar.tennisclubreservationapp.court.storage.CourtRepository;
+import cz.omar.tennisclubreservationapp.surface.mappers.SurfaceToDatabaseMapper;
+import cz.omar.tennisclubreservationapp.surface.storage.SurfaceDao;
+import cz.omar.tennisclubreservationapp.surface.storage.SurfaceEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TennisClubReservationAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TennisClubReservationAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(TennisClubReservationAppApplication.class, args);
+
+        SurfaceToDatabaseMapper surfaceToDatabaseMapper = context.getBean(SurfaceToDatabaseMapper.class);
+        SurfaceDao surfaceDao = context.getBean(SurfaceDao.class);
+        SurfaceEntity surfaceEntity = new SurfaceEntity();
+        surfaceEntity.setName("clay");
+        surfaceEntity.setRentPerMinute(15.5f);
+        surfaceDao.create(surfaceEntity);
+
+        CourtRepository courtRepository = context.getBean(CourtRepository.class);
+        CourtEntity courtEntity = new CourtEntity();
+        courtEntity.setName("Wembley 1");
+        courtEntity.setSurfaceEntity(surfaceEntity);
+        Court newCourt = courtRepository.create(courtEntity);
+
+        System.out.println("Court created: " + newCourt);
     }
 
 }
