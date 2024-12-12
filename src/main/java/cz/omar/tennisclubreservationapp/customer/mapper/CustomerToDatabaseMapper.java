a64b800706da273514e695e232c1bb0d109e22b0
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-12 18:35:32 +0100

    fix: add mapper to spring IoC container

--- src/main/java/cz/omar/tennisclubreservationapp/customer/mapper/CustomerToDatabaseMapper.java
+++ src/main/java/cz/omar/tennisclubreservationapp/customer/mapper/CustomerToDatabaseMapper.java
@@ -5,7 +5,7 @@ import cz.omar.tennisclubreservationapp.customer.storage.CustomerEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CustomerToDatabaseMapper {
     @Mapping(source = "id", target = "id")
     @Mapping(source = "firstName", target = "firstName")
@@ -14,6 +14,7 @@ public interface CustomerToDatabaseMapper {
     Customer entityToCustomer(CustomerEntity customerEntity);
 
     @Mapping(source = "id", target = "id")
+    @Mapping(target = "deleted", constant = "false")
     @Mapping(source = "firstName", target = "firstName")
     @Mapping(source = "lastName", target = "lastName")
     @Mapping(source = "phoneNumber", target = "phoneNumber")
