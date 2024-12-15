f6aaecd61201f757b4c872f3982ccd5264508664
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-16 00:59:10 +0100

    feat: add logout service

---
+++ src/main/java/cz/omar/tennisclubreservationapp/auth/LogoutService.java
@@ -0,0 +1,39 @@
+package cz.omar.tennisclubreservationapp.auth;
+
+import cz.omar.tennisclubreservationapp.token.storage.TokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+    private final TokenRepository tokenRepository;
+
+    @Override
+    public void logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        var storedToken = tokenRepository.getByToken(jwt);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.update(storedToken);
+            SecurityContextHolder.clearContext();
+        }
+    }
+}
