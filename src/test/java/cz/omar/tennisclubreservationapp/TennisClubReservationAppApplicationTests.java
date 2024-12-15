6ccd5692e7e7ffb7971846fb7a12f4d624342763
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-15 15:30:51 +0100

    feat: update app test

--- src/test/java/cz/omar/tennisclubreservationapp/TennisClubReservationAppApplicationTests.java
+++ src/test/java/cz/omar/tennisclubreservationapp/TennisClubReservationAppApplicationTests.java
@@ -1,13 +1,20 @@
 package cz.omar.tennisclubreservationapp;
 
+import cz.omar.tennisclubreservationapp.reservation.ReservationController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TennisClubReservationAppApplicationTests {
 
+    @Autowired
+    ReservationController reservationController;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(reservationController);
     }
 
 }
