af95d31a56d9d6add89a1129e09e47aad8324447
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-15 15:39:35 +0100

    refactor: remove unused

--- src/main/java/cz/omar/tennisclubreservationapp/common/seeding/Seeder.java
+++ src/main/java/cz/omar/tennisclubreservationapp/common/seeding/Seeder.java
@@ -11,7 +11,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
