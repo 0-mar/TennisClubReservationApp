447cf2537357e210564f916201705c63e80e3244
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-15 15:31:04 +0100

    feat: add application config

---
+++ src/main/java/cz/omar/tennisclubreservationapp/common/config/AppConfig.java
@@ -0,0 +1,9 @@
+package cz.omar.tennisclubreservationapp.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cz.omar.tennisclubreservationapp")
+public class AppConfig {
+}
