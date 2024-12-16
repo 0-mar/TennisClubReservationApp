c1faee3f4582ec6b2a3b70ace1d1138c9cb76d10
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-16 01:06:19 +0100

    feat: add authentication DTOs

---
+++ src/main/java/cz/omar/tennisclubreservationapp/auth/dto/AuthenticationDto.java
@@ -0,0 +1,15 @@
+package cz.omar.tennisclubreservationapp.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationDto {
+    private String email;
+    private String password;
+}
