7d5c2c3b278d3243b0795e6ffe095f201b6a9239
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-17 12:48:25 +0100

    feat: add exception to deal with an existing email

--- src/main/java/cz/omar/tennisclubreservationapp/common/advice/ControllerAdvice.java
+++ src/main/java/cz/omar/tennisclubreservationapp/common/advice/ControllerAdvice.java
@@ -1,16 +1,23 @@
 package cz.omar.tennisclubreservationapp.common.advice;
 
 import cz.omar.tennisclubreservationapp.common.storage.RepositoryException;
+import cz.omar.tennisclubreservationapp.user.service.EmailAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class RepositoryAdvice {
+public class ControllerAdvice {
     @ExceptionHandler(RepositoryException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     String repositoryHandler(RepositoryException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String repositoryHandler(EmailAlreadyExistsException ex) {
         return ex.getMessage();
     }
 }
