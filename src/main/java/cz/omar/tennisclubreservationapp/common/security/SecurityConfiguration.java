298d4baa060986fc0d15afa9a63ba96dacbd96fa
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-17 12:41:29 +0100

    feat: set security level for all endpoints

--- src/main/java/cz/omar/tennisclubreservationapp/common/security/SecurityConfiguration.java
+++ src/main/java/cz/omar/tennisclubreservationapp/common/security/SecurityConfiguration.java
@@ -1,24 +1,13 @@
 package cz.omar.tennisclubreservationapp.common.security;
 
-import cz.omar.tennisclubreservationapp.user.storage.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import static cz.omar.tennisclubreservationapp.user.storage.Role.ADMIN;
 import static cz.omar.tennisclubreservationapp.user.storage.Role.USER;
@@ -33,60 +22,27 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
-    private final UserRepository repository;
-    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("api/auth/**")
-                                .permitAll()
+                        req
+                                .requestMatchers(POST, "api/auth/logout").hasAnyAuthority(ADMIN.name(), USER.name())
+                                .requestMatchers(POST, "api/auth/refresh-token").hasAnyAuthority(ADMIN.name(), USER.name())
+                                .requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers(GET, "/api/courts/**").hasAnyAuthority(ADMIN.name(), USER.name())
                                 .requestMatchers("/api/courts/**").hasAuthority(ADMIN.name())
                                 .requestMatchers(GET, "/api/reservations/**").hasAnyAuthority(ADMIN.name(), USER.name())
                                 .requestMatchers(POST, "/api/reservations/**").hasAnyAuthority(ADMIN.name(), USER.name())
                                 .requestMatchers("/api/reservations/**").hasAuthority(ADMIN.name())
+                                .requestMatchers("/api/users/**").hasAuthority(ADMIN.name())
                                 .anyRequest()
                                 .authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout(logout ->
-                logout.logoutUrl("/api/auth/logout")
-                        .addLogoutHandler(logoutHandler)
-                        .logoutSuccessHandler((req, res, auth) -> SecurityContextHolder.clearContext())
-                );
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> {
-            UserDetails ud = repository.getByEmail(username);
-            if (ud == null) {
-                throw new UsernameNotFoundException("User not found");
-            }
-            return ud;
-        };
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
