28040417ccc6dd6119826e19eece1f3d0d5e15b7
Ondrej Marek <xmarek9@fi.muni.cz>
2024-12-17 12:49:18 +0100

    refactor: move method

--- src/main/java/cz/omar/tennisclubreservationapp/common/security/JwtService.java
+++ src/main/java/cz/omar/tennisclubreservationapp/common/security/JwtService.java
@@ -30,6 +30,10 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
@@ -83,10 +87,6 @@ public class JwtService {
 
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
     }
 
     private Key getSignInKey() {
